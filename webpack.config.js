4d99e3f02ecad8acdb83b66f8549905b69a28526
avignat <armand@vignat.org>
2017-12-05 10:58:03 +0100

    Webpack update
    Former-commit-id: 68801c6215c762922da23ba48a99617596c78795

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,40 @@
 const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
-  entry: './static/js/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'static/dist/js')
-  }
+    entry: './static/js/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'static/dist/js')
+    },
+    plugins: [
+        new UglifyJsPlugin()
+    ],
+    module: {
+        // rules: [
+        //     {
+        //         test: /\.jsx?$/,
+        //         include: [
+        //             path.resolve(__dirname, "static/js")
+        //         ],
+        //         loader: "babel-loader",
+        //         // the loader which should be applied, it'll be resolved relative to the context
+        //         // -loader suffix is no longer optional in webpack2 for clarity reasons
+        //         // see webpack 1 upgrade guide
+        //         options: {
+        //             presets: ["es2015"]
+        //         }
+        //     }
+        // ]
+    },
+    resolve: {
+        // options for resolving module requests
+        // (does not apply to resolving to loaders)
+        modules: [
+            "node_modules",
+            path.resolve(__dirname, "static/js")
+        ],
+        // directories where to look for modules
+        extensions: [".js"]
+    }
 };
